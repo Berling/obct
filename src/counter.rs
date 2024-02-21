f031b3864d8b5ce320448221dca9638b20f85b1d
Georg Schaefer <schaefg@hochschule-trier.de>
2024-02-21 22:42:49 +0100

    feat: add counter

--- src/counter.rs
+++ src/counter.rs
@@ -1,4 +1,88 @@
-use chrono::{Datelike, NaiveDate};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+    sync::atomic::{AtomicUsize, Ordering},
+    time::UNIX_EPOCH,
+};
+
+use chrono::{Datelike, NaiveDate, NaiveDateTime, Utc};
+
+pub struct Counter {
+    inner: AtomicUsize,
+}
+
+impl Counter {
+    const FILE_NAME: &'static str = "counter";
+
+    pub fn new() -> Self {
+        Self::ensure_file_exists();
+        Self {
+            inner: Self::read_counter(),
+        }
+    }
+
+    pub fn get(&self) -> usize {
+        self.inner.load(Ordering::SeqCst)
+    }
+
+    pub fn update(&self) {
+        let amount = month_diff(Utc::now().naive_local().date(), self.last_modified()) as usize;
+        self.increase(amount);
+    }
+
+    pub fn increase(&self, amount: usize) {
+        let new_value = self
+            .inner
+            .fetch_add(amount, Ordering::SeqCst)
+            .checked_add(amount)
+            .expect("amount plus current count value less or equal to usize max");
+        Self::write_counter(new_value);
+    }
+
+    pub fn decrease(&self, amount: usize) {
+        let new_value = self
+            .inner
+            .fetch_sub(amount, Ordering::SeqCst)
+            .checked_sub(amount)
+            .expect("amount less or equal to the current count value");
+        Self::write_counter(new_value);
+    }
+
+    fn last_modified(&self) -> NaiveDate {
+        let path = Path::new(Self::FILE_NAME);
+        let last_modfied = path.metadata().unwrap().modified().unwrap();
+        let millis = last_modfied.duration_since(UNIX_EPOCH).unwrap().as_millis();
+        NaiveDateTime::from_timestamp_millis(millis as i64)
+            .unwrap()
+            .date()
+    }
+
+    fn ensure_file_exists() {
+        let path = Path::new(Self::FILE_NAME);
+        if !path.exists() {
+            let mut file = File::create(path).expect("counter file");
+            file.write_all(b"0").expect("write counter");
+            file.flush().expect("write file to disk");
+        }
+    }
+
+    fn read_counter() -> AtomicUsize {
+        let path = Path::new(Self::FILE_NAME);
+        let mut file = File::open(path).expect("counter file");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("file contents");
+        AtomicUsize::new(contents.parse::<usize>().expect("valid counter"))
+    }
+
+    fn write_counter(value: usize) {
+        let path = Path::new(Self::FILE_NAME);
+        let mut file = File::create(path).expect("counter file");
+        file.write_all(value.to_string().as_bytes())
+            .expect("write counter");
+        file.flush().expect("write file to disk");
+    }
+}
 
 fn month_diff(from: NaiveDate, to: NaiveDate) -> u32 {
     debug_assert!(from <= to);
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
 mod counter;
 
+use std::{sync::Arc, time::Duration};
+
 use axum::{
-    response::{Html, IntoResponse}, routing::get, Router
+    extract::State,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
 };
+use counter::Counter;
+use tokio::{task, time};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new()
-    .route("/", get(root))
-    .route("/heading", get(heading))
-    .route("/note", get(note))
-    .route("/beer", get(beer))
-    .route("/music", get(music));
+    let counter = Arc::new(Counter::new());
 
-    // run our app with hyper, listening globally on port 3000
+    let cloned_counter = counter.clone();
+    let forever = task::spawn(async move {
+        let mut interval = time::interval(Duration::from_secs(5));
+
+        loop {
+            interval.tick().await;
+            cloned_counter.update();
+        }
+    });
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/heading", get(heading))
+        .route("/note", get(note))
+        .route("/beer", get(beer))
+        .route("/music", get(music))
+        .route("/value", get(value))
+        .with_state(counter.clone())
+        .route("/reset", get(reset))
+        .with_state(counter);
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+    forever.await.unwrap();
 }
 
 async fn root() -> Html<&'static str> {
@@ -49,4 +71,12 @@ async fn music() -> impl IntoResponse {
         ([(axum::http::header::CONTENT_TYPE, "audio/wav")]),
         include_bytes!("music.wav"),
     )
+}
+
+async fn value(State(state): State<Arc<Counter>>) -> String {
+    state.get().to_string()
+}
+
+async fn reset(State(state): State<Arc<Counter>>) {
+    state.decrease(state.get());
 }
